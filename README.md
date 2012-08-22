ad5f1d19ab89a5dc5cf4ceb62185dff6353879d7
kleingeist <kleingeist@googlemail.com>
2012-08-22 22:12:26 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 AutoloaderServiceProvider
 =========================
+
+
+Simple transisitonal Service Provider mapping the deprecated Silex autoloader to Composer.
+
+
+Silex replaced its own autoloader in favour of the one configured with composer.
+Unfortunately this will break many ServiceProviders, that are not under active development and makes it impossible to add namespaces dynamically.
+For example https://github.com/mjakubowski/nutwerk-orm-extension has to register namespaces dependent on the config.
+
+This Service Provider can work as an interim solution until more of the Third Party Providers are updated and can provide dynamic extension of namespaces.
+
+
+
+Usage
+-----
+
+Add ae35/autoloader-service-provider as a requirement to your composer.json and register the Service Provider in Silex
+
+```php
+<?php
+
+$loader = require_once __DIR__ . '/../vendor/autoload.php';
+
+$app->register(new ae35\Provider\ComposerAutoloaderServiceProvider($loader));
+```
---
+++ src/ae35/Provider/ComposerAutoloaderServiceProvider.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace ae35\Provider;
+
+use Silex\Application;
+use Silex\ServiceProviderInterface;
+
+use Composer\Autoload\ClassLoader;
+
+class ComposerAutoloaderServiceProvider implements ServiceProviderInterface
+{
+	protected $loader;
+	
+	function __construct(ClassLoader $loader) 
+	{
+		$this->loader = $loader;
+	}
+	
+    public function register(Application $app)
+    {
+		$self = $this;
+		
+		$app['autoloader'] = function() use ($self) {
+			return $self;
+		};
+    }
+
+    public function boot(Application $app)
+    {
+    }
+    
+    /**
+	* Registers a namespace.
+	*
+	* @param string $namespace The namespace
+	* @param array|string $paths The location(s) of the namespace
+	*
+	*/
+    public function registerNamespace($namespace, $paths) 
+    {
+		$this->loader->add($namespace, $paths);
+	}
+	
+	/**
+	 * Overloading to pass calls to the ClassLoader
+	 */
+	public function __call($name, $arguments)
+    {
+		if (!method_exists($this->loader, $name)) {
+			throw new BadMethodCallException();
+		}
+		
+		return call_user_func_array(array($this->loader, $name), $arguments);
+    }
+
+	
+}
